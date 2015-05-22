078d0f850adf36c87163c96ae161ce4bf89bb218
kyriaki93 <kyriaki93@gmail.com>
2015-05-22 14:05:32 +0200

    final update

--- js/signinCtrl.js
+++ js/signinCtrl.js
@@ -6,7 +6,9 @@ dinnerPlannerApp.controller('signinCtrl', function ($scope,Dinner) {
 		$scope.fname = "";
 		$scope.lname = "";
 		$scope.number = "";
+		$scope.adr = "";
 		
+		//get tables from firebase
 		var ref = new Firebase("https://dazzling-torch-7020.firebaseio.com");
 		var refUser = new Firebase("https://dazzling-torch-7020.firebaseio.com/Users");
 		
@@ -19,8 +21,9 @@ dinnerPlannerApp.controller('signinCtrl', function ($scope,Dinner) {
 				if (error) {
 			  		document.getElementById("state").innerHTML = ("Error creating user:"+ error);
 				} else {
+					//if creation of user is successfull --> put following user infomation in table "Users"
 					var newUserRef = refUser.push();
-  					newUserRef.set({user:$scope.email, fname:$scope.fname, lname:$scope.lname, number:$scope.number, id:newUserRef.key()});
+  					newUserRef.set({user:$scope.email, fname:$scope.fname, lname:$scope.lname, number:$scope.number, adr:$scope.adr, id:newUserRef.key()});
     				
     				title = 'Yay!';
     				content = 'You have successfully creted an account!';
